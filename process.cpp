594972bdd46b147d9b92ddcd38be03a12c7cc247
Hmmsien <98779692+Hmmsien@users.noreply.github.com>
2022-03-01 23:04:04 -0500

    Update process.cpp

--- process.cpp
+++ process.cpp
@@ -6,10 +6,11 @@ int main()
  	// Creating a child process if fork() return 0.
         if(!fork()){ 
         printf("I am the parent process. PID: %d\n", getpid()); 
-        if(!fork()){ // Create new child process under the previous child process.
+        if(!fork()){ 
+         // Create new child process under the previous child process.
             printf("I am the grandchild process. PID:%d\n", getpid()); 
         }
-    } else {
-        printf("I am the grandparent process. PID:%d\n", getpid()); //This is the parent process.
+    } else { //This is the parent process.
+        printf("I am the grandparent process. PID:%d\n", getpid()); 
     }
 }
