09f97cd337b65bf5b55d87a6d1a157ff77d5e42b
zbbxh <hmyyqxi@gmail.com>
2022-03-16 19:55:37 -0400

    update

--- Semaphores/Resource.java
+++ Semaphores/Resource.java
@@ -2,6 +2,7 @@ package Semaphores;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
+import java.lang.management.ManagementFactory;
 
 public class Resource extends Thread{
     
@@ -22,17 +23,17 @@ public void run() {
     
     switch (num) {
         case 1: {//1-->Printer is requested
-            System.out.println("A Printer is requested.");
+            System.out.println("A Printer is requested. by process " + ManagementFactory.getRuntimeMXBean().getName());
             s = rm.printers;
             break;
         }
         case 2: {//2-->Plotter is requested
-            System.out.println("A Plotter is requested.");
+            System.out.println("A Plotter is requested. by process " + ManagementFactory.getRuntimeMXBean().getName());
             s = rm.plotters;
             break;
         }
         case 3: {//3-->Scanner is requested
-            System.out.println("A Scanner is requested.");
+            System.out.println("A Scanner is requested. by process " + ManagementFactory.getRuntimeMXBean().getName());
             s = rm.scanners;
         break;
         }
@@ -42,7 +43,7 @@ public void run() {
     boolean done = false;
     while(!done) {
         if(s.tryAcquire()) {//Resource is available
-            System.out.println("Request is a Success!");
+            System.out.println("Request is a Success!\n");
         try {
             //Sleep for random time between 1 to 3 seconds
             Thread.sleep(ThreadLocalRandom.current().nextInt(1, 3 + 1)*1000);
@@ -52,7 +53,7 @@ public void run() {
         s.release();
         done=true;
         }else {//Resource is not available
-            System.out.println("Request is a Failure!");
+            System.out.println("Request is a Failure!\n");
             try {
                 //Sleep for random time between 2 to 4 seconds
                 Thread.sleep(ThreadLocalRandom.current().nextInt(2, 4 + 1)*1000);
@@ -61,5 +62,6 @@ public void run() {
             }
         }
     }
+    
 }
 }
